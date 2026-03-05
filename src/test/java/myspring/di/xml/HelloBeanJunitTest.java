b4b42893977f042c99cea0a4f3e2cbeabbd6bbdc
Eojinn <aj41212@gmail.com>
2026-03-05 09:52:16 +0900

    HelloBeanJunitTest update

--- src/test/java/myspring/di/xml/HelloBeanJunitTest.java
+++ src/test/java/myspring/di/xml/HelloBeanJunitTest.java
@@ -1,14 +1,22 @@
 package myspring.di.xml;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class HelloBeanJunitTest {
+	ApplicationContext context;
+	
+	@BeforeEach
+	void init() {
+		context = new GenericXmlApplicationContext("classpath:hello-bean.xml");
+		
+	}
+	
 	
 	@Test
 	void helloBean() {
-		ApplicationContext context = new GenericXmlApplicationContext("classpath:hello-bean.xml");
 		
 	}
 }
